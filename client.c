3358f85902c4214e82651c45b76190fc60828c27
Rustamjon Yusupov <ryusupov@2-E-6.42heilbronn.de>
2024-04-19 14:15:59 +0200

    implemented ft_atoi function to convert the string representation of PID into an integer value, and pid_validation to insure that the PID contains only numeric characters.

--- client.c
+++ client.c
@@ -6,11 +6,50 @@
 /*   By: ryusupov <ryusupov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 14:30:24 by ryusupov          #+#    #+#             */
-/*   Updated: 2024/04/19 12:28:13 by ryusupov         ###   ########.fr       */
+/*   Updated: 2024/04/19 14:09:11 by ryusupov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	negative;
+	int	result;
+
+	i = 0;
+	negative = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 32))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (negative * result);
+}
+
+int	pid_validation(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
